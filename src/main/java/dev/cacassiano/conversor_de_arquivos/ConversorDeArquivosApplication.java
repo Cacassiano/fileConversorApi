bb74adf56de2692fbe6c5534249dbc703ae30da7
Cassiano <saurinho.64.ss@gmail.com>
2025-05-12 12:57:24 -0300

    first commit

---
+++ src/main/java/dev/cacassiano/conversor_de_arquivos/ConversorDeArquivosApplication.java
@@ -0,0 +1,13 @@
+package dev.cacassiano.conversor_de_arquivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDeArquivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorDeArquivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cacassiano/conversor_de_arquivos/ConversorDeArquivosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cacassiano.conversor_de_arquivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorDeArquivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
